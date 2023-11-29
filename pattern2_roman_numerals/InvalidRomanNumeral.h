a150f7abaada9cd477465d3dd3af9b81276c9e2e
martinw <waplington@yahoo.com>
2023-11-29 00:25:26 +0000

    Removed unnecessary public specifiers and corrected headers

--- pattern2_roman_numerals/InvalidRomanNumeral.h
+++ pattern2_roman_numerals/InvalidRomanNumeral.h
@@ -1,14 +1,13 @@
 #pragma once
 
-#include <ostream>
 #include <stdexcept>
 #include <string>
+#include <utility>
 
-struct InvalidRomanNumeral : public std::runtime_error
+struct InvalidRomanNumeral : std::runtime_error
 {
   std::string invalid_numeral_;
 
-public:
   InvalidRomanNumeral(std::string invalid_numeral, std::string message) :
     std::runtime_error(std::move(message)),
     invalid_numeral_(std::move(invalid_numeral))
