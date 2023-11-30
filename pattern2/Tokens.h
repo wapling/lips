bfac4f05103b2e1cc81ee079ce2929efb4749d02
martinw <waplington@yahoo.com>
2023-11-30 19:23:37 +0000

    Added constants for token literals

--- pattern2/Tokens.h
+++ pattern2/Tokens.h
@@ -9,6 +9,10 @@ int const COMMA  = 3;
 int const LBRACK = 4;
 int const RBRACK = 5;
 
+char const COMMA_CHAR  = ',';
+char const LBRACK_CHAR = '[';
+char const RBRACK_CHAR = ']';
+
 char const* const get_token_name(int x);
 
 std::ostream& operator<<(std::ostream& ostrm, Token const& token);
