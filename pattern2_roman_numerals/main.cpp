ad77e7b853d03b7ef6de14b0265a4652a8ff0370
martinw <waplington@yahoo.com>
2023-12-12 18:03:01 +0000

    Corrected exception handling.

--- pattern2_roman_numerals/main.cpp
+++ pattern2_roman_numerals/main.cpp
@@ -32,8 +32,7 @@ void LoopOverInput()
     }
     catch (InvalidCharacter const& ex)
     {
-      cout << "Invalid Roman Numeral: " << input 
-           << " has the following error:\n" << ex.what() << '\n';
+      cout << "Invalid Character: " << input << '\n' << ex.what() << '\n';
     }
   }
 }
@@ -41,7 +40,16 @@ void LoopOverInput()
 
 int main(int argc, char* argv[])
 {
-  LoopOverInput();
+  try
+  {
+    LoopOverInput();
+    return 0;
+  }
+  catch (std::exception const& ex)
+  {
+    std::cerr << "Error: " << ex.what() << '\n';
+    return 1;
+  }
 }
 
 
