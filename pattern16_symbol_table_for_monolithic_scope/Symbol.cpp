98089f44ccdffe119e0dadd3d89fa8220bd956ed
martinw <waplington@yahoo.com>
2024-01-03 23:17:49 +0000

    Corrected C++ indemtation

--- pattern16_symbol_table_for_monolithic_scope/Symbol.cpp
+++ pattern16_symbol_table_for_monolithic_scope/Symbol.cpp
@@ -3,15 +3,14 @@
 #include <sstream>
 
 std::string Symbol::to_string()
+{
+  if (type_)
   {
-    if (type_)
-    {
-      std::ostringstream;
-      oss << '<' << get_name() << ':' << type_->get_name() << '>';
-      return oss.str();
-    }
-    else
-      return get_name();
+    std::ostringstream oss;
+    oss << '<' << get_name() << ':' << type_->get_name() << '>';
+    return oss.str();
   }
-};
+  else
+    return get_name();
+}
 
